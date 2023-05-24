f4f54c86d8602c2cec43eeab440950ea70ca51cc
Sandeep Kadam <sandeepkadam1798.sk@gmail.com>
2023-05-24 23:55:23 +0530

    first Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// // let firstName="sandeep";
+// // let LastName="kadaam";
+// // let MidleName="subhash";
+
+// // document.getElementById("p1").innerHTML="first name is  :" +firstName;
+// // document.getElementById("p2").innerHTML="first Lastname is  :" +LastName;
+// // document.getElementById("p3").innerHTML="first Midlename is  :" +MidleName;
+
+
+
+// // document.getElementById("Submit").onclick=function(){
+// // Username=document.getElementById("User").value;
+// // console.log(Username)
+// // document.getElementById("Usernames").innerHTML="helllo   :" + Username;
+// // }
+// // let User1;
+// // document.getElementById("Submit1").onclick=function(){
+// // User1=document.getElementById("text1").value;
+// // console.log(User1)
+// // document.getElementById("User1").innerHTML="helllo   :" + User1;
+// // }
+
+// document.getElementById("Submit1").onclick = function() {
+//     let User1 = document.getElementById("text1").value;
+//     console.log(User1);
+//     document.getElementById("User1").innerHTML = "hello: " + User1;
+//   };
